c44ecba7d6815851b23571609ab4d65bc64260f9
THIENMAP <thienmapad190606@gmail.com>
2022-05-19 00:36:28 +0700

    moi

---
+++ app/src/main/java/com/example/foodapp/activity/ActivityDangNhap.java
@@ -0,0 +1,17 @@
+package com.example.foodapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.foodapp.R;
+
+public class ActivityDangNhap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_nhap);
+
+    }
+}
---
+++ app/src/main/java/com/example/foodapp/model/Giohang.java
@@ -0,0 +1,52 @@
+package com.example.foodapp.model;
+
+public class Giohang {
+    int idsp;
+    int soluong;
+    String tensp;
+    long gia;
+    String hinhanh;
+
+    public Giohang(){
+    }
+
+    public int getIdsp() {
+        return idsp;
+    }
+
+    public void setIdsp(int idsp) {
+        this.idsp = idsp;
+    }
+
+    public int getSoluong() {
+        return soluong;
+    }
+
+    public void setSoluong(int soluong) {
+        this.soluong = soluong;
+    }
+
+    public String getTensp() {
+        return tensp;
+    }
+
+    public void setTensp(String tensp) {
+        this.tensp = tensp;
+    }
+
+    public long getGia() {
+        return gia;
+    }
+
+    public void setGia(long gia) {
+        this.gia = gia;
+    }
+
+    public String getHinhanh() {
+        return hinhanh;
+    }
+
+    public void setHinhanh(String hinhanh) {
+        this.hinhanh = hinhanh;
+    }
+}
